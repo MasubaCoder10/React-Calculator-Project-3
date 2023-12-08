106130d8fe90d97f18534fc0fa4281ec5d666a1e
Sofiane <sofiane@iMac-de-kalssi.local>
2023-12-08 23:57:02 +0100

    add two component for the fonctionality

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.css'
 export default function Buttons(props) {
   return (
-    <div className= {`buttons  ${props.content === "0" ? "zero": ""} 
+    <div onClick={()=>{props.OnPressClick(props.content)}} className= {`buttons  ${props.content === "0" ? "zero": ""} 
     ${props.content === "±" || props.content === "AC" || props.content === "%"? "ops":""}
     ${props.content === "÷" || props.content === "x" || props.content === "-" || props.content === "+" || props.content === "="? "operator":""}`}>
         {props.content} 
--- src/Components/CalculatorApp.jsx
+++ src/Components/CalculatorApp.jsx
@@ -1,35 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Buttons from './Buttons'
 import './CalculatorApp.css'
 
 export default function CalculatorApp() {
-  return (
+    const [value, setValue] = useState(0);
+    const [number, setNumber] = useState(null);
+    const [operator, setOperator] = useState(null);
+     const handlePress = content =>{
+        let numbers = (value)
+        if(content === "AC"){
+            setValue(0)
+            setNumber(null);
+            setOperator(null);
+            return;
+        }
+        if(content === "±"){
+            setValue((numbers * -1).toString() )
+            return;
+        }
+        if(content === "%"){
+            setValue((numbers / 100).toString())
+            return;
+        }
+
+        if(content === "+"){      
+            setNumber((value).toString());
+            setValue(0);
+            setOperator("+");
+            return;
+        }
+    
+        if(content === "-"){
+            setNumber((value).toString())
+            setOperator("-")
+            setValue(0);
+            return;
+        }
+        if(content === "÷"){
+            setNumber((value).toString())
+            setOperator("÷")
+            setValue(0);
+            return;
+        }
+        if(content === "x"){
+            setNumber((value).toString())
+            setOperator("x")
+            setValue(0);
+            return;
+        } 
+
+          if(content === "="){
+            if(operator === "+"){
+                let result= Number(number) + Number(value)
+                setValue(result)
+                return;
+            }else if(operator === "-"){
+                let result= Number(number) - Number(value)
+                setValue(result)
+                return;
+            }
+            else if(operator === "x"){
+                let result= Number(number) * Number(value)
+                setValue(result)
+                return;
+            }
+             else if(operator === "÷"){
+                let result= Number(number) / Number(value)
+                setValue(result)
+                return;
+            }
+
+            /* if(value[value.length -1] === "."){
+
+            } */
+
+            
+            
+        }
+        
+        
+        setValue((numbers + content));    
+}
+ 
+    const arrayButton = ["AC", "±", "%", "÷", "7", "8","9", "x", "4", "5", "6", "-",
+     "1","2", "3", "+", "0", ".", "="];
+
+   return (
     <div className='calculator'>
-    <div className="result">0</div>
+    <div className="result">{value}</div>
     <div className='CalculatorApp'>
-        <Buttons content = {"AC"} />
-        <Buttons content = {"±"} />
-        <Buttons content = {"%"} />
-        <Buttons content = {"÷"} />
-
-        <Buttons content = {"7"} />
-        <Buttons content = {"8"} />
-        <Buttons content = {"9"} />
-        <Buttons content = {"x"} />
-
-        <Buttons content = {"4"} />
-        <Buttons content = {"5"} />
-        <Buttons content = {"6"} />
-        <Buttons content = {"-"} />
-
-        <Buttons content = {"1"} />
-        <Buttons content = {"2"} />
-        <Buttons content = {"3"} />
-        <Buttons content = {"+"} />
-
-        <Buttons content = {"0"} />
-        <Buttons content = {"."} />
-        <Buttons content = {"="} />
+    {
+        arrayButton.map((item, index)=>{
+            return <Buttons key={"buttonsCalculator" + index} OnPressClick = {handlePress} content = {item} />
+        })
+    }
         
     </div>
     </div>
